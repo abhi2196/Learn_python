79d8c3179d0c64900503d58c415f8f3e1feb256a
abhi2196 <abhicool673@gmail.com>
2017-08-07 15:40:57 +0530

    Adding args to target

--- Multithreading/First_thread.py
+++ Multithreading/First_thread.py
@@ -1,12 +1,12 @@
 import threading
 
 
-def f():
-	print 'Inside Thread Function'
+def f(id):
+	print 'Inside Thread Function Called by Thread ID: %d' % (id)
 	return
 
 if __name__ == '__main__':
 	for i in range(3):
-		t = threading.Thread(target=f)
+		t = threading.Thread(target=f, args=(i,))
 		t.start()
 
