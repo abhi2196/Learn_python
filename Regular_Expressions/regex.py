1b4c0106d17ee0af798284bdd1c7024d596adc4b
Abhishek Shukla <ashukla@hortonworks.com>
2018-11-08 01:05:54 +0530

    re module examples

---
+++ Regular_Expressions/regex.py
@@ -0,0 +1,27 @@
+import re
+
+def extract_course_times():
+    """Write a regular expression that returns a list of timestamps:
+        ['01:47', '32:03', '41:51', '27:48', '05:02']"""
+    flask_course = ('Introduction 1 Lecture 01:47'
+                    'The Basics 4 Lectures 32:03'
+                    'Getting Technical!  4 Lectures 41:51'
+                    'Challenge 2 Lectures 27:48'
+                    'Afterword 1 Lecture 05:02')
+    return re.findall(r'\d{2}:\d{2}', flask_course)
+
+
+def get_all_hashtags_and_links():
+    """Write a regular expression that returns this list:
+       ['http://pybit.es/requests-cache.html', '#python', '#APIs']"""
+    tweet = ('New PyBites article: Module of the Week - Requests-cache '
+             'for Repeated API Calls - http://pybit.es/requests-cache.html '
+             '#python #APIs')
+    return re.findall(r'http[a-z:\/\.\-]+|#\w+', tweet)
+
+
+def match_first_paragraph():
+    """Write a regular expression that returns  'pybites != greedy' """
+    html = ('<p>pybites != greedy</p>'
+            '<p>not the same can be said REgarding ...</p>')
+    return re.findall(r'\w+\s+!=\s+\w+', html)[0]
